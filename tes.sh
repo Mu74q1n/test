89515ca10794f55ba8dbcadc42d9da02d7389978
Mu74q1n <53370311+Mu74q1n@users.noreply.github.com>
2019-07-27 18:35:42 +0700

    Add files via upload

---
+++ tes.sh
@@ -0,0 +1,207 @@
+#!/system/bin/sh
+#language : bash
+# author : mutaqin revoer
+
+ab='\033[30;m' #ABU
+me='\033[31;1m' #MERAH
+i='\033[32;1m' #IJO
+ku='\033[33;1m' #KUNING
+bi='\033[34;1m' #BIRU
+pur='\033[35;1m' #PURPLE
+cy='\033[36;1m' #CYAN
+pu='\033[37;1m' #PUTIH
+
+clear
+echo "Starting tools /"
+sleep 0.1
+clear
+
+echo "sTarting tools -"
+sleep 0.1
+clear
+
+echo "stArting tools |"
+sleep 0.1
+clear
+
+echo "StaRting tools /"
+sleep 0.1
+clear
+
+echo "starTing tools -"
+sleep 0.1
+clear
+
+echo "startIng tools |"
+sleep 0.1
+clear
+
+echo "startiNg tools /"
+sleep 0.1
+clear
+
+echo "startinG tools -"
+sleep 0.1
+clear
+
+echo "starting Tools |"
+sleep 0.1
+clear
+
+echo "starting tOols /"
+sleep 0.1
+clear
+
+echo "starting toOls -"
+sleep 0.1
+clear
+
+echo "starting tooLs |"
+sleep 0.1
+clear
+
+echo "starting toolS /"
+sleep 0.1
+clear
+
+echo "Starting tools /"
+sleep 0.1
+clear
+
+echo "sTarting tools -"
+sleep 0.1
+clear
+
+echo "stArting tools |"
+sleep 0.1
+clear
+
+echo "StaRting tools /"
+sleep 0.1
+clear
+
+echo "starTing tools -"
+sleep 0.1
+clear
+
+echo "startIng tools |"
+sleep 0.1
+clear
+
+echo "startiNg tools /"
+sleep 0.1
+clear
+
+echo "startinG tools -"
+sleep 0.1
+clear
+
+echo "starting Tools |"
+sleep 0.1
+clear
+
+echo "starting tOols /"
+sleep 0.1
+clear
+
+echo "starting toOls -"
+sleep 0.1
+clear
+
+echo "starting tooLs |"
+sleep 0.1
+clear
+
+echo "Starting tools /"
+sleep 0.1
+clear
+
+echo "sTarting tools -"
+sleep 0.1
+clear
+
+echo "stArting tools |"
+sleep 0.1
+clear
+
+echo "StaRting tools /"
+sleep 0.1
+clear
+
+echo "starTing tools -"
+sleep 0.1
+clear
+
+echo "startIng tools |"
+sleep 0.1
+clear
+
+echo "startiNg tools /"
+sleep 0.1
+clear
+
+echo "startinG tools -"
+sleep 0.1
+clear
+
+echo "starting Tools |"
+sleep 0.1
+clear
+
+echo "starting tOols /"
+sleep 0.1
+clear
+
+echo "starting toOls -"
+sleep 0.1
+clear
+
+echo "starting tooLs |"
+sleep 0.1
+clear
+
+echo "starting toolS /"
+sleep 0.1
+clear
+echo "starting toolS /"
+sleep 0.1
+clear
+echo $i"author"    $me":" $cy"MUT4Q1N R3V03R"
+sleep 1
+echo $i"contak"   $me":"  $cy"081223963163"
+sleep 2
+echo $i"nama fb"$me":"  $cy"Mutaqin Revoer"
+sleep 3
+echo $ku"sok pilih nama bapak nya"
+echo $i"[1] Abde"
+echo $bi"[2] dudi"
+echo $me"[3] Rivan"
+echo $ku"[4] udan"
+echo $cy""
+read -p "[?] sok sok pilih anjing :" pilih
+
+if [ $pilih = "1" ]
+then
+sleep 5
+echo $cy"nama bapak nya adalah ridwan"
+fi
+
+if [ $pilih = "2" ]
+then
+sleep 5
+echo $ku"nama bapak nya adalah atep"
+fi
+
+if [ $pilih = "3" ]
+then
+sleep 5
+echo $me"nama bapaknya adalah wahid"
+fi
+
+if [ $pilih = "4" ]
+then
+sleep 5
+echo $i"nama bapak nya adalah"
+echo $lolcat"adnan"
+sleep 3
+fi
